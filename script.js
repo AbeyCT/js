962288354d8a391462eaa2aca1a39890d7535efa
AbeyTech <adejumobia3@gmail.com>
2024-08-23 11:51:29 +0100

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Toggle Navigation Menu
 const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
+const navLinks = document.getElementById('nav-links xxxxxxxxx');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
